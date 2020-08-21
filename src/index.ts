bf4aa1c89061ca957d113b14becbd37b037ac433
lakhansamani <lakhan.m.samani@gmail.com>
2020-08-22 00:51:51 +0530

    feat: add basic express server

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const main = async () => {
+  // create express app
+  const app = express();
+
+  // bind add to local port and start server
+  const port = parseInt(process.env.PORT || '3000', 10);
+  app.listen(port, () => {
+    console.log(`🚀 server started on ${port}`);
+  });
+};
+
+main();
