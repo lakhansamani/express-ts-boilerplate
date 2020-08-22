bc930131841df3889143ab2d3e26d8d2f9c46743
lakhansamani <lakhan.m.samani@gmail.com>
2020-08-22 10:31:53 +0530

    fix: update server start log

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const main = async () => {
   // bind add to local port and start server
   const port = parseInt(process.env.PORT || '3000', 10);
   app.listen(port, () => {
-    console.log(`🚀 server started on ${port}`);
+    console.log(`🚀 server started on port: ${port}`);
   });
 };
 
