547ce3b815d494701a4f0d7dacd82a0766caa8fa
lakhansamani <lakhan.m.samani@gmail.com>
2020-08-22 10:47:45 +0530

    feat: add type check for express application

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import cors from 'cors';
 
 const main = async () => {
   // create express app
-  const app = express();
+  const app: express.Application = express();
 
   // add cors middleware
   app.use(cors());
 
   // bind port and start server
-  const port = parseInt(process.env.PORT || '3000', 10);
+  const port: number = parseInt(process.env.PORT || '3000', 10);
   app.listen(port, () => {
     console.log(`🚀 server started on port: ${port}`);
   });
