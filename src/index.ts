69ff23e2bae93edd0f409cd8e3b149b656973e0f
lakhansamani <lakhan.m.samani@gmail.com>
2020-08-22 11:37:22 +0530

    feat: add json body parser

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv-safe/config';
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const main = async () => {
   // create express app
@@ -8,6 +9,9 @@ const main = async () => {
 
   // add cors middleware
   app.use(cors());
+
+  // add json body parser
+  app.use(bodyParser.json())
 
   // bind port and start server
   const port: number = parseInt(process.env.PORT || '3000', 10);
